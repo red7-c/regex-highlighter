56b699a5d70e4cbafc482079dfa87ffb64c44dbb
svrxo <info.sohaib.fares@gmail.com>
2025-11-30 01:06:01 +0100

    feat: add model structure & init functions

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -1,1 +1,40 @@
 package tui
+
+import (
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Model struct {
+	fileName  string
+	Content   string
+	ready     bool
+	Err       error
+	textInput textinput.Model
+	viewport  viewport.Model
+}
+
+func InitialModel(filepath, content string) Model {
+
+	cursorStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#acaca8ff"))
+
+	ti := textinput.New()
+	ti.Placeholder = "Type regex expression here..."
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 60
+	ti.Cursor.Style = cursorStyle
+
+	return Model{
+		fileName:  filepath,
+		Content:   content,
+		Err:       nil,
+		textInput: ti,
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return textinput.Blink
+}
