a98a2ff767ac13c2fb198aca048de24ebe0c1c3f
svrxo <info.sohaib.fares@gmail.com>
2025-12-01 17:12:32 +0100

    feat: add better input & some layout changes

--- internal/tui/update.go
+++ internal/tui/update.go
@@ -1,42 +1,79 @@
 package tui
 
 import (
-	"github.com/charmbracelet/bubbles/viewport"
+	"fmt"
+	"regexp"
+
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
+type errMsg error
+
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
-	var (
-		cmd  tea.Cmd
-		cmds []tea.Cmd
-	)
-
+	var cmd tea.Cmd
+	var cmds []tea.Cmd
 	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
-			return m, tea.Quit
-		}
 
 	case tea.WindowSizeMsg:
-		headerHeight := lipgloss.Height(m.HeaderView())
-		footerHeight := lipgloss.Height(m.FooterView())
-		verticalMarginHeight := headerHeight + footerHeight
+		m.WindowHeight = msg.Height
+		m.WindowWidth = msg.Width
+		// for the TextInput (1 line) + some margin (2 lines in this case) + spearator (1 line)
+		headerHeight := 4
+		m.Viewport.Height = m.WindowHeight - headerHeight
+		m.Viewport.Width = m.WindowWidth
+		m.TextInput.Width = m.WindowWidth - 5
+		m.Viewport, cmd = m.Viewport.Update(msg)
+		return m, nil
 
-		if !m.ready {
-			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
-			m.viewport.YPosition = headerHeight
-			m.viewport.SetContent(m.Content)
-			m.ready = true
-		} else {
-			m.viewport.Width = msg.Width
-			m.viewport.Height = msg.Height - verticalMarginHeight
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "up", "k":
+			m.Viewport.ScrollUp(1)
+		case "down", "j":
+			m.Viewport.ScrollDown(1)
+		case "esc":
+			if m.TextInput.Focused() {
+				m.TextInput.Blur()
+			}
+
+		default:
+			if !m.TextInput.Focused() && msg.Type == tea.KeyRunes {
+				cmd = m.TextInput.Focus()
+				cmds = append(cmds, cmd)
+			}
 		}
+	case errMsg:
+		m.Err = msg
+		return m, nil
 	}
-	m.viewport, cmd = m.viewport.Update(msg)
-	cmds = append(cmds, cmd)
-	m.textInput, cmd = m.textInput.Update(msg)
+
+	newTextInput, tiCmd := m.TextInput.Update(msg)
+	cmds = append(cmds, tiCmd)
+
+	if newTextInput.Value() != m.TextInput.Value() {
+		m.TextInput = newTextInput
+		pattern := m.TextInput.Value()
+
+		if pattern == "" {
+			m.CompiledRegex = nil
+			m.Err = nil
+		} else {
+			re, err := regexp.Compile(pattern)
+			if err != nil {
+				m.Err = fmt.Errorf("invalid regex expression", err)
+				m.CompiledRegex = nil
+			} else {
+				m.CompiledRegex = re
+				m.Err = nil
+			}
+		}
+	} else {
+		m.TextInput = newTextInput
+	}
+
+	m.Viewport, cmd = m.Viewport.Update(msg)
 	cmds = append(cmds, cmd)
 
 	return m, tea.Batch(cmds...)
